51c70cb5fe0e9dbe6c47cdcdb30aebfa871088c1
NinaShved <neenas@mail.ru>
2020-09-05 16:54:54 +0300

    Первый коммит

---
+++ hello.py
@@ -0,0 +1,52 @@
+# print("Привет мир!")
+# print("Привет,программист!")
+# print(2+2)
+
+#print(int(1/3)) 
+#print(10**2)
+# print(10//2)
+# a=2
+# print(a+7)
+# name="Нина"
+# print(name)
+# print(len(name[:-1]))
+# print(name[:-1])
+# a=2
+# b=0.5
+# print(a+b)
+# d="Привет {name}!".format(name=name)
+# print(d)
+# v=input("Введите число от 1 до 10 ")
+# b=float(v)
+# c=b+10
+# print(c)
+# name=input("Введите Ваше имя: ")
+# print(f"Привет, {name}! Как дела?")
+#print(float('1'))
+#print(int('2.5'))
+#print(bool(1))
+#print(bool(''))
+#print(bool(0))
+#name="Нина"
+#print(len(name[:-2]))
+#print(name[:-2])
+#numbers=[3,5,7,9,10.5,"Pyton"]
+#print(numbers)
+#print(len(numbers))
+#print(numbers[0:1])
+#print(numbers[5:6])
+#print(numbers[1:4])
+#del numbers[5]
+#print(numbers)
+#dic={"city":"Москва","temperature":"20"}
+# print(dic["city"])
+# a=float(dic["temperature"])
+# b=a-5
+# print(b)
+# dic["temperature"] = int(dic["temperature"]) - 5
+# print(dic)
+# print(dic["country"])
+#print(dic.get("country"))
+
+
+
